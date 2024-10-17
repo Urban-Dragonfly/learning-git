3f8c61c6999f844ff6afd8c0c6e87885c2f49f0d
Urban-Dragonfly <Urban_Dragonfly@outlook.com>
2024-10-17 16:50:17 +0200

    Update main.py with chaos
    Kallisti!

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ podzielne = []
 for x in range (1, 101):
     if x % 5 == 0:
         podzielne.append(x)
-
+print("Hail Eris!")
 print()
 print ("Liczby podzielne przez 5 to:")
 print(", ".join(map(str, podzielne)))
@@ -13,3 +13,4 @@ print()
 print("A ich trzecie potęgi to:")
 print(", ".join(map(str, [x**3 for x in podzielne])))
 print("Nikt się nie spodziewał Hiszpańskiej Inkwizycji!")
+print("fnord!")
